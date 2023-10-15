f1c1cf196bd3062f76c03253a421617d531e82f9
Ubuntu <ubuntu@ip-172-31-19-40.ec2.internal>
2023-10-15 19:20:57 +0000

    main file change

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("ya allah ya mohammd, Maashallkah! ya allah i love you ");
+	System.out.println("ya allah ya rasul allah");
     }
 }
 
